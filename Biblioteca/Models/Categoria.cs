edee5fc830162cc71eb769ab140dcdeb79b6dfb9
Felipe Movio <felipelsmovio@gmail.com>
2026-09-02 00:47:36 -0300

    pequeno ajuste nas entidades

--- Biblioteca/Models/Categoria.cs
+++ Biblioteca/Models/Categoria.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Biblioteca.Models;
 
 public class Categoria
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     [Required]
--- Biblioteca/Models/Emprestimo.cs
+++ Biblioteca/Models/Emprestimo.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Biblioteca.Models;
 
 public class Emprestimo
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     [Range(1, int.MaxValue)]
